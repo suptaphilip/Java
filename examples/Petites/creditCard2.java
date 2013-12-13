21335cddeaa27450e92670bad6811b5b5b972292
KellyChan <kwailamchan@hotmail.com>
2013-12-13 11:27:07 +0800

    Create creditCard2.java

---
+++ examples/Petites/creditCard2.java
@@ -0,0 +1,88 @@
+// Bluej project: lesson6/credit_card
+// Video: Credit Card Checksum 
+
+// TODO: Implement the isValid method.
+public class CreditCard
+{
+    private long cardNumber;
+
+    public CreditCard(long cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+
+    /**
+     * Calculates whether this CreditCard has a valid number.
+     * @return true if the number is valid, false if it's not.
+     */
+    public boolean isValid()
+    {
+        /* Pseudocode for isValid:
+         * sum = 0
+         * count = 0
+         * for each digit starting from the right
+         *     count ++
+         *     if count is odd
+         *         sum = sum + digit
+         *     else if (digit < 5)
+         *         sum = sum + 2 * digit
+         *     else
+         *         sum = sum + 2 * digit - 9
+         * if the last digit of the sum is zero
+         *     The card number is valid
+         */
+        long n = cardNumber;
+        int sum = 0;
+        int count = 0;
+
+        // TODO this is the code from the last question. you can use it
+        // as a starting point, but you will need to change most of it.
+        while (n > 0)
+        {
+            count++;
+            int digit = (int)(n % 10); // 
+            if (count % 2 == 1)
+            {
+                sum = sum + digit;
+            }
+            else if (digit < 5)
+            {
+                sum = sum + 2 * digit;
+            }
+            else
+            {
+                sum = sum + 2 * digit - 9;
+            }
+            n = n/10;
+        }
+   
+        if (sum % 10 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+public class CreditCardTester
+{
+    public static void main(String[] args)
+    {
+        CreditCard validCard = new CreditCard(4417123456789113L);
+        System.out.println("Actual: " + validCard.isValid());
+        System.out.println("Expected: true");
+
+        CreditCard invalidCard = new CreditCard(4417123456879113L);
+        System.out.println("Actual: " + invalidCard.isValid());
+        System.out.println("Expected: false");
+
+        CreditCard easyToRememberValidCard = new CreditCard(4111111111111111L);
+        System.out.println("Actual: " + easyToRememberValidCard.isValid());
+        System.out.println("Expected: true");
+
+    }
+}
